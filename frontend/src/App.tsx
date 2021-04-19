52bfadcfd50b0907cf53c768e121e31a568d1b3d
Jheavi <javierlaso213@hotmail.com>
2021-04-19 18:51:29 +0200

    add principal view and some general styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import configureStore from './redux/configureStore'
 import initialState from './redux/initialState'
 import Header from './components/Header/Header'
+import Principal from './views/Principal/Principal'
 require('dotenv').config()
 
 const store = configureStore(initialState)
@@ -14,6 +15,7 @@ function App () {
     <Provider store={store}>
       <BrowserRouter>
         <Header />
+        <Principal />
       </BrowserRouter>
     </Provider>
   )
