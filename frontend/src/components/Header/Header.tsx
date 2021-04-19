a3bac6612c9326e35f25f44833bffd3bab0f106e
Jheavi <javierlaso213@hotmail.com>
2021-04-19 18:47:31 +0200

    add links to header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './header.css'
 
 function Header () {
   return (
-    <header></header>
+    <header>
+      <h1>TV Shows and Movies</h1>
+      <div className="options">
+        <Link to="">
+          <h2>Movies</h2>
+        </Link>
+        <Link to="">
+          <h2>TV Shows</h2>
+        </Link>
+      </div>
+    </header>
   )
 }
 
