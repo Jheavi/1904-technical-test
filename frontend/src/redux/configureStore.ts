2b39a2d76c2b497941a449ef2ae3e7ecbad9ad58
Jheavi <javierlaso213@hotmail.com>
2021-04-19 13:10:47 +0200

    add initial redux config

---
+++ frontend/src/redux/configureStore.ts
@@ -0,0 +1,13 @@
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+export default function configureStore (initialState: {} | undefined) {
+  const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
+  )
+}
