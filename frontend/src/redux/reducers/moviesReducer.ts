785d29111ffa06e3b56bffa1b54457f0075677e7
Jheavi <javierlaso213@hotmail.com>
2021-04-20 12:22:28 +0200

    add load similar movies case in moviesReducer

--- frontend/src/redux/reducers/moviesReducer.ts
+++ frontend/src/redux/reducers/moviesReducer.ts
@@ -6,6 +6,8 @@ export default function moviesReducer (state = initialState.moviesReducer, actio
   switch (action.type) {
     case actionTypes.LOAD_POPULAR_MOVIES:
       return { ...state, popularMovies: action.movieList }
+    case actionTypes.LOAD_SIMILAR_MOVIES:
+      return { ...state, similarMovies: action.similarMovies.slice(0, 6) }
     default:
       return state
   }
