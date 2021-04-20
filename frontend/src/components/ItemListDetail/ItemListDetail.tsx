a5a2f691c97753b3da7421e18a382573d2c3ccd6
Jheavi <javierlaso213@hotmail.com>
2021-04-20 12:18:57 +0200

    add showAdditionalInfo in itemListDetail compo to show or not vote average

--- frontend/src/components/ItemListDetail/ItemListDetail.tsx
+++ frontend/src/components/ItemListDetail/ItemListDetail.tsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom'
 import movieDbUrls from '../../constants/urls'
 import './ItemListDetail.css'
 
-export default function ItemListDetail ({ item }: any) {
+export default function ItemListDetail ({ item, showAdditionalInfo = true }: any) {
   return (
     <li className="item-container" >
       <Link to={`/detail/${item.id}`} className="detail-link">
         <img src={`${movieDbUrls.images}${item.poster_path}`} alt={item.title || item.name}/>
         <h3>{item.title || item.name}</h3>
       </Link>
-      <span>Vote average: {item.vote_average}</span>
-      <span className="small-text">(from {item.vote_count} votes)</span>
+      {showAdditionalInfo &&
+      <>
+        <span>Vote average: {item.vote_average}</span>
+        <span className="small-text">(from {item.vote_count} votes)</span>
+      </>
+      }
     </li>
   )
 }
