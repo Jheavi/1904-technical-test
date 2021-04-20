da7e2098515cff74f6183e981c8fa3df0d45b221
Jheavi <javierlaso213@hotmail.com>
2021-04-20 14:28:48 +0200

    refactor of moviesReducer tests, 100% coverage

--- frontend/src/redux/reducers/moviesReducer.test.ts
+++ frontend/src/redux/reducers/moviesReducer.test.ts
@@ -4,8 +4,10 @@ import moviesReducer from './moviesReducer'
 
 describe('Given a moviesReducer function', () => {
   let fakeMovieList: [{ title: string }]
+  let fakeMovie: { title: string }
   beforeEach(() => {
     fakeMovieList = [{ title: 'fakeTitle' }]
+    fakeMovie = { title: 'fakeTitle' }
   })
 
   describe('When an undefined state is passed', () => {
@@ -25,7 +27,43 @@ describe('Given a moviesReducer function', () => {
 
       const reducerResult = moviesReducer(undefined, fakeAction)
 
-      expect(reducerResult).toEqual({ popularMovies: fakeMovieList })
+      expect(reducerResult).toEqual({
+        popularMovies: fakeMovieList,
+        similarMovies: []
+      })
+    })
+  })
+
+  describe('When an action with type LOAD_MOVIE_DETAIL is passed', () => {
+    test('Then should return the movie object in a property called movie', () => {
+      const fakeAction = {
+        type: actionTypes.LOAD_MOVIE_DETAIL,
+        movie: fakeMovie
+      }
+
+      const reducerResult = moviesReducer(undefined, fakeAction)
+
+      expect(reducerResult).toEqual({
+        popularMovies: [],
+        similarMovies: [],
+        movie: fakeMovie
+      })
+    })
+  })
+
+  describe('When an action with type LOAD_SIMILAR_MOVIES is passed', () => {
+    test('Then should return the similar movies list in a property called similarMovies', () => {
+      const fakeAction = {
+        type: actionTypes.LOAD_SIMILAR_MOVIES,
+        similarMovies: fakeMovieList
+      }
+
+      const reducerResult = moviesReducer(undefined, fakeAction)
+
+      expect(reducerResult).toEqual({
+        popularMovies: [],
+        similarMovies: fakeMovieList
+      })
     })
   })
 })
